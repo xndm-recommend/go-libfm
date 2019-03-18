37bfed78c1497cf4444d65f263ca76f61c3de588
xndm-recommend <1050099567@qq.com>
2019-03-18 14:15:50 +0800

    modify api

--- libfm.go
+++ libfm.go
@@ -17,47 +17,62 @@ import (
 	"github.com/xndm-recommend/go-utils/errors_"
 )
 
+type LibFMClient struct {
+	ModelPath string
+	FMModel   *Model
+}
+
+type LibLROptions struct {
+	Model_path string // 模型路径
+}
+
 type Model struct {
 	cModel *C.struct_tag_fm_model
 }
 
-func (c *Model) Predict(data []string) []float64 {
-	fmt.Println("leb data", len(data))
+func (c *LibFMClient) Predict(data []string) []float64 {
 	p := make([]float64, 0, len(data))
 	for _, s := range data {
 		var cmsg *C.char = C.CString(s)
 		defer C.free(unsafe.Pointer(cmsg))
-		p = append(p, float64(C.call_predict(c.cModel, cmsg)))
+		p = append(p, float64(C.call_predict(c.FMModel.cModel, cmsg)))
 	}
 	return p
 }
 
-func (c *Model) LoadModel(filename string) {
+func (c *LibFMClient) LoadModel(filename string) {
 	var cmsg *C.char = C.CString(filename)
 	defer C.free(unsafe.Pointer(cmsg))
-	if ok := int(C.call_loadModel(c.cModel, cmsg)); ok != 1 {
-		errors_.CheckFatalErr(errors.New(fmt.Sprintf("Can't load model from %v", filename)))
+	if ok := int(C.call_loadModel(c.FMModel.cModel, cmsg)); ok != 1 {
+		errors_.CheckErrSendEmail(errors.New(fmt.Sprintf("Can't load model from %v", filename)))
 	}
 }
 
-func InitModel(filename string) *Model {
+func (c *LibFMClient) InitModel(filename string) error {
 	model := C.call_create()
-	if model == nil {
-		errors_.CheckFatalErr(errors.New(fmt.Sprintf("Can't load model from %v", filename)))
-	}
 	m := &Model{
 		cModel: model,
 	}
-	m.LoadModel(filename)
-	return m
+	m.cModel.LoadModel(filename)
+	if model == nil {
+		return errors.New(fmt.Sprintf("Can't load model from %v", filename))
+	}
+	return nil
 }
 
-//func main() {
-//	//router := gin.Default()
-//	//router.GET("/aaaa/aaaa", get_word)
-//	//router.Run(":8116")
-//
-//	m := InitModel("model.out")
-//	a := []string{"-1 1:0.2 3:0.1 8:1 7:0.256312"}
-//	fmt.Println(m.Predict(a))
-//}
+func (c *LibFMClient) init() {
+	// 模型初始化
+	errors_.CheckFatalErr(c.InitModel(c.ModelPath))
+}
+
+func NewLibFMClient(opt *LibLROptions) (*LibFMClient, error) {
+	c := &LibFMClient{
+		ModelPath: opt.Model_path,
+		FMModel:   new(Model),
+	}
+	c.init()
+	if c.FMModel == nil {
+		errors_.CheckFatalErr(fmt.Errorf("Can't load model from %v", c.ModelPath))
+	}
+	return c, nil
+}
